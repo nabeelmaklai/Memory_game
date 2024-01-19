c2affc7fb30bb6f28c9e9e69aa288b33b8113472
Nabeel Maklai <nabilmaklai@hotmail.com>
2024-01-20 01:19:47 +0300

    make the blink work

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let board = document.querySelectorAll('.box')
 
 const generateRandom = () => {
   let len = pattern.length
-  let num = Math.floor(Math.random() * len + 1)
+  let num = Math.floor(Math.random() * len)
   pattern.push(num)
 }
 
@@ -26,24 +26,53 @@ generateRandom()
 generateRandom()
 generateRandom()
 generateRandom()
+generateRandom()
+generateRandom()
+generateRandom()
+generateRandom()
+generateRandom()
 
-const addClass = () => {
-  for (let i = 0; i < board.length; i++) {
-    document.querySelectorAll('.box')[i].classList.add('black')
-  }
-}
+// const addClass = () => {
+//   for (let i = 0; i < board.length; i++) {
+//     document.querySelectorAll('.box')[i].classList.add('black')
+//   }
+// }
+
+// const blink = () => {
+//   setTimeout(() => {})
+//   for (let i = 0; i < pattern.length; i++) {
+//     setTimeout(function timer() {
+//       document.querySelectorAll('.box')[pattern[i]].classList.remove('black')
+//       setTimeout(() => {
+//         if (!board[i].classList.contains('black')) {
+//           board[i].classList.add('black')
+//         }
+//       }, 1500)
+//     }, i * 2000)
+//   }
+// }
 
 const blink = () => {
-  for (let i = 0; i < pattern.length; i++) {
-    setTimeout(function timer() {
-      document.querySelectorAll('.box')[pattern[i]].classList.remove('black')
+  setTimeout(() => {
+    for (let i = 0; i < pattern.length; i++) {
       setTimeout(() => {
-        if (board[i].classList.contains('black')) {
-          board[i].classList.remove('black')
-        }
-      }, 1500)
-    }, i * 2000)
-  }
+        //console.log(i)
+        document.querySelectorAll('.box')[pattern[i]].classList.add('black')
+        setTimeout(() => {
+          if (
+            document
+              .querySelectorAll('.box')
+              [pattern[i]].classList.contains('black')
+          ) {
+            console.log(i)
+            document
+              .querySelectorAll('.box')
+              [pattern[i]].classList.remove('black')
+          }
+        }, 500)
+      }, i * 1000)
+    }
+  }, 500)
 }
 
 // Stack overflow https://stackoverflow.com/questions/3583724/how-do-i-add-a-delay-in-a-javascript-loop
@@ -60,7 +89,10 @@ const selector = () => {
   }
 }
 
-addClass()
+// addClass()
+blink()
+
+console.log(pattern)
 
 // const setInt = () => {
 //   // x = pattern.length
@@ -86,7 +118,6 @@ addClass()
 //setInt()
 // blink()
 
-// console.log(pattern)
 // document.querySelectorAll('.box')[3].classList.toggle('black')
 
 // Have the function increment the score for each correct pattern match
