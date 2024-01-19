80d3526ab7ce3ade8aba403d3efc35a5103e8a8e
Nabeel Maklai <nabilmaklai@hotmail.com>
2024-01-19 21:13:55 +0300

    add blink functionality

--- script.js
+++ script.js
@@ -26,13 +26,27 @@ generateRandom()
 generateRandom()
 generateRandom()
 generateRandom()
-generateRandom()
+
+const addClass = () => {
+  for (let i = 0; i < board.length; i++) {
+    document.querySelectorAll('.box')[i].classList.add('black')
+  }
+}
 
 const blink = () => {
   for (let i = 0; i < pattern.length; i++) {
-    document.querySelectorAll('.box')[pattern[i]].classList.toggle('black')
+    setTimeout(function timer() {
+      document.querySelectorAll('.box')[pattern[i]].classList.remove('black')
+      setTimeout(() => {
+        if (board[i].classList.contains('black')) {
+          board[i].classList.remove('black')
+        }
+      }, 1500)
+    }, i * 2000)
   }
 }
+
+// Stack overflow https://stackoverflow.com/questions/3583724/how-do-i-add-a-delay-in-a-javascript-loop
 
 // Have the player select one of the squares
 
@@ -46,34 +60,33 @@ const selector = () => {
   }
 }
 
-selector('X')
+addClass()
 
-attBlink()
+// const setInt = () => {
+//   // x = pattern.length
+//   for (let i = 0; i < pattern.length; i++) {
+//     setInterval(blink, 2000)
+//   }
+// }
 
-const setInt = () => {
-  // x = pattern.length
-  for (let i = 0; i < pattern.length; i++) {
-    setInterval(blink, 2000)
-  }
-}
+// let i = 0
 
-let i = 0
-
-const itteration = () => {
-  if (i < 4) {
-    console.log(i)
-    blink()
-    i++
-    setTimeout(() => {
-      blink()
-      itteration()
-    }, 3000)
-  }
-}
-itteration()
+// const itteration = () => {
+//   if (i < 4) {
+//     console.log(i)
+//     blink()
+//     i++
+//     setTimeout(() => {
+//       blink()
+//       itteration()
+//     }, 3000)
+//   }
+// }
+// itteration()
 //setInt()
+// blink()
 
-console.log(pattern)
+// console.log(pattern)
 // document.querySelectorAll('.box')[3].classList.toggle('black')
 
 // Have the function increment the score for each correct pattern match
