6edcb044befa2bb3c8edc117052b878f4463bd5e
Nabeel Maklai <nabilmaklai@hotmail.com>
2024-01-20 18:09:27 +0300

    add player move

--- script.js
+++ script.js
@@ -3,60 +3,27 @@
 let pattern = []
 let playerPattern = []
 let board = document.querySelectorAll('.box')
+let score = 0
+let wrongMove = 0
 
 // Have JS generate random numbers from 1 to 6
 
 const generateRandom = () => {
-  let len = pattern.length
-  let num = Math.floor(Math.random() * len)
+  let num = Math.floor(Math.random() * 6)
   pattern.push(num)
 }
 
 // Add a function to light up parts of the board
 
-const attBlink = () => {
-  for (let i = 0; i < board.length; i++) {
-    board[i].addEventListener('click', () => {
-      document.querySelectorAll('.box')[i].classList.toggle('black')
-    })
-  }
-}
-
-generateRandom()
-generateRandom()
-generateRandom()
-generateRandom()
-generateRandom()
-generateRandom()
-generateRandom()
-generateRandom()
-generateRandom()
-
-// const addClass = () => {
-//   for (let i = 0; i < board.length; i++) {
-//     document.querySelectorAll('.box')[i].classList.add('black')
-//   }
-// }
-
-// const blink = () => {
-//   setTimeout(() => {})
-//   for (let i = 0; i < pattern.length; i++) {
-//     setTimeout(function timer() {
-//       document.querySelectorAll('.box')[pattern[i]].classList.remove('black')
-//       setTimeout(() => {
-//         if (!board[i].classList.contains('black')) {
-//           board[i].classList.add('black')
-//         }
-//       }, 1500)
-//     }, i * 2000)
-//   }
-// }
+// generateRandom()
+// generateRandom()
+// generateRandom()
+// generateRandom()
 
 const blink = () => {
   setTimeout(() => {
     for (let i = 0; i < pattern.length; i++) {
       setTimeout(() => {
-        //console.log(i)
         document.querySelectorAll('.box')[pattern[i]].classList.add('black')
         setTimeout(() => {
           if (
@@ -64,13 +31,12 @@ const blink = () => {
               .querySelectorAll('.box')
               [pattern[i]].classList.contains('black')
           ) {
-            console.log(i)
             document
               .querySelectorAll('.box')
               [pattern[i]].classList.remove('black')
           }
         }, 500)
-      }, i * 1000)
+      }, i * 1500)
     }
   }, 500)
 }
@@ -79,63 +45,89 @@ const blink = () => {
 
 // Have the player select one of the squares
 
-const selector = () => {
+const checkPattern = () => {
+  for (let i = 0; i < board.length; i++) {
+    if (playerPattern[i] !== pattern[i]) {
+      wrongMove = wrongMove + 1
+    } else {
+      console.log('correct')
+    }
+  }
+  console.log('wrong move')
+}
+
+// const rmClass=(i)=>{
+//   (let i = 0; i < board.length; i++)
+//     document
+//       .querySelectorAll('.box')
+//       [pattern[i]].classList.contains('black')
+//   ) {
+//     document
+//       .querySelectorAll('.box')
+//       [pattern[i]].classList.remove('black')
+//   }
+// }
+
+const rmClass = () => {
+  for (let i = 0; i < board.length; i++) {
+    if (document.querySelectorAll('.box')[i].classList.contains('black')) {
+      document.querySelectorAll('.box')[i].classList.remove('black')
+    }
+  }
+}
+const playerSelect = () => {
+  let move = pattern.length
+  let j = 0
   for (let i = 0; i < board.length; i++) {
     board[i].addEventListener('click', () => {
-      //board[i].innerHTML = 'X'
-      playerPattern.push(board[i].innerHTML - 1)
-      console.log(playerPattern)
+      if (j < move) {
+        board[i].classList.add('black')
+        playerPattern.push(parseInt(board[i].innerHTML) - 1)
+        j = j + 1
+        console.log(playerPattern)
+        setTimeout(() => {
+          rmClass()
+        }, 350)
+      }
+      checkPattern()
     })
   }
 }
 
-// addClass()
+generateRandom()
 blink()
+playerSelect()
+checkPattern()
 
-console.log(pattern)
-
-// const setInt = () => {
-//   // x = pattern.length
-//   for (let i = 0; i < pattern.length; i++) {
-//     setInterval(blink, 2000)
-//   }
-// }
-
-// let i = 0
-
-// const itteration = () => {
-//   if (i < 4) {
-//     console.log(i)
-//     blink()
-//     i++
-//     setTimeout(() => {
-//       blink()
-//       itteration()
-//     }, 3000)
-//   }
-// }
-// itteration()
-//setInt()
-// blink()
-
-// document.querySelectorAll('.box')[3].classList.toggle('black')
-
-// Have the function increment the score for each correct pattern match
-
-// const changeClass = () => {
-//   document.querySelectorAll('.box')[1].classList.toggle('black')
-//document.body.classList.toggle('grey')
-//}
 // const blink = () => {
+//   setTimeout(() => {
+//     for (let i = 0; i < pattern.length; i++) {
+//       setTimeout(function timer() {
+//         document.querySelectorAll('.box')[pattern[i]].classList.remove('black')
+//         setTimeout(() => {
+//           if (!board[i].classList.contains('black')) {
+//             board[i].classList.add('black')
+//           }
+//         }, 1500)
+//       }, i * 2000)
+//     }
+//   }, 500)
+// }
+
+// const attBlink = () => {
 //   for (let i = 0; i < board.length; i++) {
-//     board[i].addEventListener('click', changeClass)
-//   }
-// }
-
-// const blink = () => {
-//   board.forEach((num, i) => {
-//     num.addEventListener('click', () => {
+//     board[i].addEventListener('click', () => {
 //       document.querySelectorAll('.box')[i].classList.toggle('black')
 //     })
-//   })
+//   }
+// }
+// const selector = () => {
+//   for (let i = 0; i < board.length; i++) {
+//     board[i].addEventListener('click', () => {
+//       playerPattern.push(parseInt(board[i].innerHTML) - 1)
+//       checkPattern()
+//       console.log(playerPattern)
+//       console.log(score)
+//     })
+//   }
 // }
