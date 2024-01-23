6b4daa427f209413c1ecadd62506c41655e23fa5
Nabeel Maklai <nabilmaklai@hotmail.com>
2024-01-23 21:30:14 +0300

    new difficult modes

--- script.js
+++ script.js
@@ -6,10 +6,6 @@ let board = document.querySelectorAll('.box')
 let scoreDisplay = document.querySelector('.score')
 let reset = document.querySelector('.reset')
 let score = 0
-let wrongMove = 0
-let eachClick = 0
-let currentLevel = 1
-let previousLevel = 0
 
 // Have JS generate random numbers from 1 to 5
 
@@ -36,10 +32,10 @@ const blink = () => {
               .querySelectorAll('.box')
               [pattern[i]].classList.remove('black')
           }
-        }, 500)
-      }, i * 1500)
+        }, 400)
+      }, i * 1000)
     }
-  }, 500)
+  }, 200)
 }
 
 reset.addEventListener('click', () => {
@@ -71,8 +67,10 @@ const playerSelect = () => {
         }, 350)
         if (j === move) {
           if (JSON.stringify(playerPattern) != JSON.stringify(pattern)) {
-            // location.reload()
-            console.log('it worked')
+            alert(
+              `Wrong pattern, Game Over.your score  was ${score}, The game will now restart`
+            )
+            location.reload()
           } else {
             setTimeout(() => {
               playerPattern = []
@@ -91,50 +89,3 @@ const playerSelect = () => {
 generateRandom()
 blink()
 playerSelect()
-
-// const checkPattern = () => {
-//   for (let i = 0; i < board.length; i++) {
-//     if (playerPattern[i] != pattern[i]) {
-//       return true
-//     } else {
-//       return false
-//       console.log('now thats good')
-//     }
-//   }
-// }
-// checkButton.addEventListener('click', () => {
-//   if (eachClick < 1) {
-//     if (JSON.stringify(playerPattern) != JSON.stringify(pattern)) {
-//       console.log('wrong pattern')
-//       playerPattern = []
-//       console.log(playerPattern)
-//       eachClick++
-//     } else {
-//       previousLevel++
-//       score += 10
-//       console.log(score)
-//       eachClick++
-//       scoreDisplay.innerHTML = 'Score: ' + score
-//     }
-//   }
-// })
-// nextPattern.addEventListener('click', () => {
-//   if (previousLevel === currentLevel) {
-//     eachClick = 0
-//     playerPattern = []
-//     generateRandom()
-//     blink()
-//     playerSelect()
-//     previousLevel = 0
-//   }
-// })
-// const checkPattern = () => {
-//   if (playerPattern !== pattern) {
-//     wrongMove = wrongMove + 1
-//   } else {
-//     generateRandom()
-//     blink()
-//     playerSelect()
-//     console.log(pattern)
-//   }
-// }
